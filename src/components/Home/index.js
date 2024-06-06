59e9b3961388f3d88591cd5d3c74642b9c5818bb
Swethaks285 <swethaks285@gmail.com>
2024-06-06 11:42:17 +0000

    first commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,26 +1,26 @@
-import {Component} from 'react'
-import Login from '../Login'
+import {Link} from 'react-router-dom'
+
+import Header from '../Header'
+
 import './index.css'
 
-class Home extends Component {
-  render() {
-    return (
-      <div className="bg-full">
-        <div className="bg-cen">
-          <h1 className="h1">Find The Job That Fits Your Life</h1>
-          <p className="p">
-            Millions of people are searching for jobs, salary information,
-            company reviews. Find the job that fits your abilities and
-            potential.
-          </p>
-          <button type="button" className="btn">
+const Home = () => (
+  <>
+    <Header />
+    <div className="home-container">
+      <div className="home-content">
+        <h1 className="home-heading">Find The Job That Fits Your Life</h1>
+        <p className="home-description">
+          Millions of people are searching for jobs, salary information, company
+          reviews. Find the job that fits your abilities and potential.
+        </p>
+        <Link to="/jobs">
+          <button type="button" className="shop-now-button">
             Find Jobs
           </button>
-        </div>
-        <Login />
+        </Link>
       </div>
-    )
-  }
-}
-
+    </div>
+  </>
+)
 export default Home
